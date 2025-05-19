068121489b3a4b008d2a005ba7f673ab6acd450a
Dilgo-dev <dilgopierre@gmail.com>
2025-05-19 12:32:14 +0200

    feat: Integrate TypeORM with SQLite and update environment configuration for database connection

---
+++ backend/src/database/datasource.js
@@ -0,0 +1,8 @@
+import { DataSource } from "typeorm";
+import "dotenv/config";
+
+export const AppDataSource = new DataSource({
+    type: "sqlite",
+    database: process.env.DB_PATH,
+    synchronize: true,
+});
