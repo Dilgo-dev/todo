c01e5d7d40e40e7bf6a12ac59d9c99d4fe216ff8
Dilgo-dev <dilgopierre@gmail.com>
2025-05-19 13:26:34 +0200

    chore: Integrate Prisma for database management and update Docker configurations to support generated client files

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -1,18 +1,7 @@
 import express from 'express';
-import { AppDataSource } from './database/datasource.js';
-import 'dotenv/config';
-import 'reflect-metadata';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-AppDataSource.initialize()
-  .then(() => {
-    console.info('🐶 Database is running');
-  })
-  .catch(err => {
-    console.error('❌ Database is not running', err);
-  });
 
 app.use(express.json());
 
