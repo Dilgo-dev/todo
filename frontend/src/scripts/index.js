44a8aa72119e3c7a7f248eda3dccbfb967e02132
Dilgo-dev <dilgopierre@gmail.com>
2025-05-19 16:17:25 +0200

    feat: Create registration page and implement registration functionality with error handling

--- frontend/src/scripts/index.js
+++ frontend/src/scripts/index.js
@@ -2,6 +2,8 @@ import addTodo from './addTodo.js';
 import renderTodos from './renderTodo.js';
 import { input, button } from './document.js';
 import me from './auth/me.js';
+import logout from './auth/logout.js';
+
 
 button.addEventListener('click', () => {
     addTodo(input.value);
@@ -16,9 +18,18 @@ input.addEventListener('keypress', e => {
 renderTodos();
 
 const user = await me();
-
 if (user.email) {
     email.innerHTML = user.email;
+    const logoutButton = document.createElement('button');
+    logoutButton.innerHTML = 'Déconnexion';
+    logoutButton.addEventListener('click', async () => {
+        try {
+            await logout();
+        } catch (error) {
+            console.error('Logout failed:', error);
+        }
+    });
+    userdata.appendChild(logoutButton);
     auth.remove();
 } else {
     userdata.remove();
