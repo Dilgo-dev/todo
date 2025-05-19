fc72e2ec77abe76597ac7a0bcb020e5c9a98adf4
Dilgo-dev <dilgopierre@gmail.com>
2025-05-19 12:36:08 +0200

    refactor: Standardize code formatting in server and datasource files for consistency

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -1,26 +1,28 @@
-import express from "express";
-import { AppDataSource } from "./database/datasource.js";
-import "dotenv/config";
-import "reflect-metadata"
+import express from 'express';
+import { AppDataSource } from './database/datasource.js';
+import 'dotenv/config';
+import 'reflect-metadata';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-AppDataSource.initialize().then(() => {
-    console.log("🐶 Database is running");
-}).catch((err) => {
-    console.error("❌ Database is not running", err);
-});
+AppDataSource.initialize()
+  .then(() => {
+    console.log('🐶 Database is running');
+  })
+  .catch(err => {
+    console.error('❌ Database is not running', err);
+  });
 
 app.use(express.json());
 
-app.get("/api", (req, res) => {
-    res.send("Hello World");
+app.get('/api', (req, res) => {
+  res.send('Hello World');
 });
 
 app.listen(PORT, () => {
-    console.log(`🐶 Server is running on port http://localhost:${PORT}`);
-    if (process.env.NODE_ENV === "development") {
-        console.log(`🔧 Mode développement activé`);
-    }
+  console.log(`🐶 Server is running on port http://localhost:${PORT}`);
+  if (process.env.NODE_ENV === 'development') {
+    console.log(`🔧 Mode développement activé`);
+  }
 });
